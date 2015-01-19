b190da6459b2e6d3689929e855aefec1c55a0945
SergeyKorus <szgrebelny@korusconsulting.ru>
2015-01-19 19:01:14 +0400

    add file

---
+++ war/src/main/java/ru/korus/tmis/pdm/repositories/pdmfiles/PdmFilesRepository.java
@@ -0,0 +1,15 @@
+package ru.korus.tmis.pdm.repositories.pdmfiles;
+
+import ru.korus.tmis.pdm.entities.pdm.Term;
+import ru.korus.tmis.pdm.entities.pdmfiles.PdmFiles;
+import ru.korus.tmis.pdm.repositories.pdm.PrivateKeyAndHistoryRepository;
+
+/**
+ * Author:      Sergey A. Zagrebelny <br>
+ * Date:        07.10.2014, 12:33 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+public interface PdmFilesRepository extends PrivateKeyAndHistoryRepository<PdmFiles> {
+
+}
